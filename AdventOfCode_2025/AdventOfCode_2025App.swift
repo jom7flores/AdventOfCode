b09a6c8c099599c172c04e390a5f0561fb5dce31
Josue Flores <josue.flores@wizeline.com>
2025-12-01 20:06:41 -0700

    Initial Commit

---
+++ AdventOfCode_2025/AdventOfCode_2025App.swift
@@ -0,0 +1,17 @@
+//
+//  AdventOfCode_2025App.swift
+//  AdventOfCode_2025
+//
+//  Created by Josue Flores Inchaurregui on 12/1/25.
+//
+
+import SwiftUI
+
+@main
+struct AdventOfCode_2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdventOfCode_2025/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AdventOfCode_2025
+//
+//  Created by Josue Flores Inchaurregui on 12/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
